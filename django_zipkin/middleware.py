dcd4a577856415439ba018ccc4efd77c537db844
Zoltan Nagy <abesto@abesto.net>
2014-09-03 17:41:50 +0200

    Never ever throw exception during request / response processing

--- django_zipkin/middleware.py
+++ django_zipkin/middleware.py
@@ -39,26 +39,32 @@ class ZipkinMiddleware(object):
         self.logger = logging.getLogger(settings.ZIPKIN_LOGGER_NAME)
 
     def process_request(self, request):
-        self.store.clear()
-        data = self.request_parser.get_zipkin_data(request)
-        if data.trace_id is None:
-            data.trace_id = self.id_generator.generate_trace_id()
-        data.parent_span_id = data.span_id
-        data.span_id = self.id_generator.generate_span_id()
-        self.store.set(data)
-        self.api.set_rpc_name(request.method)
-        self.api.record_event(SERVER_RECV)
-        self.api.record_key_value(constants.ANNOTATION_HTTP_URI, request.get_full_path())
+        try:
+            self.store.clear()
+            data = self.request_parser.get_zipkin_data(request)
+            if data.trace_id is None:
+                data.trace_id = self.id_generator.generate_trace_id()
+            data.parent_span_id = data.span_id
+            data.span_id = self.id_generator.generate_span_id()
+            self.store.set(data)
+            self.api.set_rpc_name(request.method)
+            self.api.record_event(SERVER_RECV)
+            self.api.record_key_value(constants.ANNOTATION_HTTP_URI, request.get_full_path())
+        except Exception:
+            logging.root.exception('ZipkinMiddleware.process_request failed')
 
     def process_response(self, request, response):
-        data = self.store.get()
-        if data.trace_id is None:
-            self.process_request(request)
-            self.api.record_event(constants.ANNOTATION_NO_DATA_IN_LOCAL_STORE)
-        self.api.record_event(SERVER_SEND)
-        self.api.record_key_value(constants.ANNOTATION_HTTP_STATUSCODE, response.status_code)
-        if data.is_tracing():
-            self.logger.info(self.api.build_log_message())
+        try:
+            data = self.store.get()
+            if data.trace_id is None:
+                self.process_request(request)
+                self.api.record_event(constants.ANNOTATION_NO_DATA_IN_LOCAL_STORE)
+            self.api.record_event(SERVER_SEND)
+            self.api.record_key_value(constants.ANNOTATION_HTTP_STATUSCODE, response.status_code)
+            if data.is_tracing():
+                self.logger.info(self.api.build_log_message())
+        except Exception:
+            logging.root.exception('ZipkinMiddleware.process_response failed')
         return response
 
     def _build_trace(self):
